9953b3c6acbaab540005476fbb0c46ee269858a0
USER <USER@DESKTOP-8UI3NE1>
2021-06-02 17:44:15 +0700

    Lop

---
+++ src/main/java/view/lop/LopSetTableModel.java
@@ -0,0 +1,85 @@
+package view.lop;
+
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+
+import javax.swing.table.AbstractTableModel;
+
+import bean.DBConnection;
+
+
+
+@SuppressWarnings("serial")
+public class LopSetTableModel extends AbstractTableModel {
+	private DBConnection conn = new DBConnection();
+	private ResultSet rs;
+	private ResultSetMetaData rsmd;
+	private final String title[] = {"Mã lớp", "Giảng viên", "Tên lớp", "Sỉ số lớp"};
+	
+	public LopSetTableModel() {
+		// TODO Auto-generated constructor stub
+		try {
+			conn.getConnection();
+			String query = "select * from lop";
+			rs = conn.excuted(query);
+			rsmd = rs.getMetaData();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public void disconnect() {
+		conn.closeConnection();
+	}
+
+	@Override
+	public int getRowCount() {
+		try {
+			rs.last();
+			return rs.getRow();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		try {
+			return rsmd.getColumnCount();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		try {
+			if (rs.absolute(rowIndex + 1))
+				return rs.getObject(columnIndex + 1);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	@Override
+	public String getColumnName(int column) {
+		// TODO Auto-generated method stub
+//		try {
+//			return rsmd.getColumnName(column + 1);
+//		} catch (SQLException e) {
+//			// TODO Auto-generated catch block
+//			e.printStackTrace();
+//		}
+//		return null;
+		return title[column];
+	}
+
+}
