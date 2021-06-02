53b3cff39b8184584c2192c7ba8296bfcbb916be
trinhthao203 <trinhtthao203@gmail.com>
2021-06-02 10:55:00 +0700

    Sửa model giảng viên

--- src/main/java/bean/GiangVien.java
+++ src/main/java/bean/GiangVien.java
@@ -1,5 +1,36 @@
 package bean;
 
-public class GiangVien {
+/**
+ * Model Giảng viên
+ * @author Trịnh Thanh Thảo
+ *
+ */
 
+public class GiangVien {
+	private String maGiangVien;
+	private String tenGiangVien;
+	
+	public GiangVien() {
+		super();
+	}
+	
+	public GiangVien(String maGiangVien, String tenGiangVien) {
+		super();
+		this.maGiangVien=maGiangVien;
+		this.tenGiangVien=tenGiangVien;
+	}
+	
+	public String getmaGiangVien() {
+		return maGiangVien;
+	}
+	
+	public String gettenGiangVien() {
+		return tenGiangVien;
+	}
+	
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return getmaGiangVien() + "\t" + gettenGiangVien();
+	}
 }
